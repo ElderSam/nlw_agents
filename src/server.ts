a9855f58531976e62f987fcc29bfaee894d51232
ElderSam <eldersamuel98@gmail.com>
2025-07-12 21:08:43 -0300

    feat: create route GET /rooms

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { fastifyCors } from '@fastify/cors';
 import { fastify } from 'fastify';
 
 import {
@@ -6,10 +7,8 @@ import {
     type ZodTypeProvider
 } from 'fastify-type-provider-zod';
 
-import { fastifyCors } from '@fastify/cors';
-
 import { env } from './env.ts';
-import { sql } from './db/connection.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -21,9 +20,11 @@ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 
-app.get('/health', (req, res) => {
+app.get('/health', (_, res) => {
     res.send('ok')
 })
+
+app.register(getRoomsRoute);
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP server running!')
